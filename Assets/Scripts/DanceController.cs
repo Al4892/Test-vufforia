3a83013f1186a45a32ec524346c14adacc084aa2
Al4892 <alan.iniguez@uteca.edu.mx>
2025-05-21 20:31:40 -0600

    xd

--- Assets/Scripts/DanceController.cs
+++ Assets/Scripts/DanceController.cs
@@ -4,16 +4,25 @@ using UnityEngine.Events;
 public class DanceController : MonoBehaviour
 {
     [SerializeField]
+    private Animator _characterAnimator;
+    [SerializeField]
     private UnityEvent _onSelectDance;
     [SerializeField]
     private UnityEvent _onDanceSelected;
+    private SoundData _CurrentSoundData;
 
     public void ActiveSelectDance()
     {
         _onSelectDance?.Invoke();
     }
-    public void onSelectedDance()
+    public void onSelectedDance(SoundData soundData)
     {
+        _CurrentSoundData = soundData;
         _onDanceSelected?.Invoke();
     }
+    public void StartDance()
+    {
+        _characterAnimator.Play(_CurrentSoundData.AnimationName);
+        SoundManager.instance.PlayMusic(_CurrentSoundData.MusiName);
+     }
 }
---
+++ Assets/Scripts/SoundData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundData", menuName = "Scriptable Objects/SoundData")]
+public class SoundData : ScriptableObject
+{
+    public string MusiName;
+    public string AnimationName;
+    public TextAsset notesConfig;
+}
